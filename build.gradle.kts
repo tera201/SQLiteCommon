2d4cec5bdbc20dfc003bc4b8008444374223e649
rnaryshkin <r.naryshkin99@gmail.com>
2025-03-28 14:29:59 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "org.tera201"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-simple:1.7.36")
+    implementation("org.xerial:sqlite-jdbc:3.45.3.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(19)
+}
